ad8a745b7051db8022c7fcbb6e8917aed791fb33
Will Badart <Badart_William@bah.com>
2022-08-13 14:31:08 -0700

    Implement complex vending machine
    Also add `run-trace` helper for sequencing a process through a list of
    events to see if it traces or bleeps.

--- csp.rkt
+++ csp.rkt
@@ -1,5 +1,7 @@
 #lang lazy
 
+(require lazy/force)
+
 (define (STOP _)
   'BLEEP)
 
@@ -36,12 +38,37 @@
       ([x (f x)])
     x))
 
+(define (run-trace t P)
+  (when
+      (cons? t)
+    (let* [(t0 (car t))
+          (next (P t0))]
+      (if
+       (eq? next 'BLEEP)
+       (print 'BLEEP)
+       (begin
+         (! (print t0))
+         (run-trace (cdr t) next))))))
 
 (define VM
   (prefix 'coin (prefix 'choc STOP)))
 
 (define VMS
   (fix (lambda (X) (prefix 'coin (prefix 'choc X)))))
+
+(define VMC
+  (fix
+   (lambda (X)
+     (choice2
+      'in2p
+      (choice2
+       'large X
+       'small (prefix 'out1p X))
+
+      'in1p
+      (choice2
+       'small X
+       'in1p (choice2 'large X 'in1p STOP))))))
 
 (define CT
   (choice2 'up STOP 'right (prefix 'right (prefix 'right (prefix 'up STOP)))))
